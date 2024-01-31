fd830df2db926ef6889bc4a285b327a0f9374afd
Christopher Ksiazak <cksiazak@gmail.com>
2024-01-31 11:54:35 -0700

    updating styling and layout

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Speech Synthesis
 
 Experimenting with [Speech Synthesis](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis)
+
---
+++ src/components/SpeechSynthesis.tsx
@@ -0,0 +1,13 @@
+import './styles/SpeechSynthesis.scss'
+
+
+const SpeechSynthesis = () => {
+  return (
+    <form>
+      <label htmlFor="speech-synthesis">Speech Synthesis</label>
+      <textarea name='speech-synthesis'/>
+    </form>
+  )
+}
+
+export default SpeechSynthesis
