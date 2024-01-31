5541b7c35546bab919a38da9ee65054bfa80d3b0
Christopher Ksiazak <cksiazak@gmail.com>
2024-01-31 14:25:59 -0700

    integrated language selection

--- src/components/SpeechSynthesis.tsx
+++ src/components/SpeechSynthesis.tsx
@@ -6,7 +6,7 @@ import { useSpeechSynthesis } from '../hooks/useSpeechSynthesis'
 
 const SpeechSynthesis = () => {
   const [text, setText] = useState('')
-  const { speak, voices, setVoice } = useSpeechSynthesis()
+  const { speak, voices, voice, languages, setVoice, setLanguage } = useSpeechSynthesis()
 
   const handleTextAreaUpdate = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value)
@@ -21,19 +21,27 @@ const SpeechSynthesis = () => {
         placeholder='Type some text here...'
       />
       <div>
-      <select name='voices' onChange={(e) => setVoice(e.target.value)}>
-        <option value="">Default</option>
-        {voices.map((voice, i) => (
-          <option key={`${voice}_${i}`} value={voice}>{voice}</option>
-        ))}
-      </select>
-      <button onClick={(e) => {
-        e.preventDefault()
-        speak(text)
-      }}>
-        Speak
-      </button>
-        </div>
+        <select name='language' onChange={e => setLanguage(e.target.value)}>
+          <option value="">All</option>
+          {languages.map((lang, i) => (
+            <option key={`${lang}_${i}`} value={lang}>
+              {lang}
+            </option>
+          ))}
+        </select>
+        <select name='voices' value={voice} onChange={(e) => setVoice(e.target.value)}>
+          <option value="">Default</option>
+          {voices.map((voice, i) => (
+            <option key={`${voice}_${i}`} value={voice}>{voice}</option>
+          ))}
+        </select>
+        <button onClick={(e) => {
+          e.preventDefault()
+          speak(text)
+        }}>
+          Speak
+        </button>
+      </div>
     </form>
   )
 }
--- src/hooks/useSpeechSynthesis.ts
+++ src/hooks/useSpeechSynthesis.ts
@@ -1,25 +1,69 @@
-const speech = new SpeechSynthesisUtterance()
+import { useCallback, useEffect, useMemo, useState } from "react"
 
 export const useSpeechSynthesis = () => {
-
-  const voices = window?.speechSynthesis?.getVoices() || []
-  const voiceNames = voices?.map?.(voiceName => voiceName.name) || []
+  const speech = useMemo(() => new SpeechSynthesisUtterance(), [])
+  const voices = useMemo(() => window?.speechSynthesis?.getVoices() || [], [])
+  const languages = [...new Set(voices?.map?.(voice => voice.lang))]
+  
+  const [selectedLanguage, setSelectedLanguage] = useState('')
+  const [selectedVoice, setSelectedVoice] = useState('')
+  const [availableVoices, setAvailableVoices] = useState<string[]>([])
 
   const speak = (text: string) => {
     speech.text = text
+    speech.rate = 1
     window.speechSynthesis.speak(speech)
-    console.log(speech)
   }
 
-  const setVoice = (name: string) => {
+  const setLanguage = (lang: string) => {
+    let newLang = lang ?? null
+    setSelectedLanguage(newLang)
+  }
+
+  const filterVoices = useCallback((lang: string) => {
+    const selectableVoices = selectedLanguage ? voices?.filter(voice => voice.lang === lang) : voices
+    const voiceNames = selectableVoices?.map?.(voiceName => voiceName.name) || []
+    return voiceNames
+  }, [selectedLanguage, voices])
+
+  const handleAvailableVoices = useCallback((lang: string) => {
+    const voiceNames = filterVoices(lang)
+    setAvailableVoices(voiceNames)
+  }, [filterVoices])
+
+  useEffect(() => {
+    handleAvailableVoices(selectedLanguage)
+  }, [handleAvailableVoices, selectedLanguage])
+
+  
+  const setVoice = useCallback((name: string) => {
     let newVoice = name ?? null
-    const selectedVoice = voices.find(item => item.name === newVoice) as SpeechSynthesisVoice
-    speech.voice = selectedVoice
+    const voice = voices.find(item => item.name === newVoice) as SpeechSynthesisVoice
+    speech.voice = voice
+    setSelectedVoice(newVoice ?? '')
+  }, [speech, voices])
+
+  useEffect(() => {
+    const firstVoiceInLanguage = selectedLanguage ? filterVoices(selectedLanguage)[0] : ''
+    setVoice(firstVoiceInLanguage)
+  }, [availableVoices, filterVoices, selectedLanguage, setVoice])
+
+  const pause = () => {
+    window.speechSynthesis.pause()
+  }
+
+  const resume = () => {
+    window.speechSynthesis.resume()
   }
 
   return {
     speak,
     setVoice,
-    voices: voiceNames,
+    voices: availableVoices,
+    voice: selectedVoice,
+    pause,
+    resume,
+    languages,
+    setLanguage,
   }
 }
